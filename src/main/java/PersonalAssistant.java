fb48ef648213c64a1cfbc28340ec8c483160a959
A Maadhava Selvan <maadhavaselvan@gmail.com>
2026-05-15 23:46:37 +0530

    Remove target and src from tracking

--- src/main/java/PersonalAssistant.java
+++
@@ -1,6 +0,0 @@
-import dev.langchain4j.service.SystemMessage;
-
-public interface PersonalAssistant {
-    @SystemMessage("You are a helpful personal assistant. You have tools to send emails and send Discord messages. If the user asks you to do either, use the tools provided.")
-    String chat(String userMessage);
-}
